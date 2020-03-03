730242554a4b1accd21b82b23dcb3f65b5103dae
Benjamin A Ajisefinni Jr <aj.no.limit.1@gmail.com>
2020-03-03 03:57:32 +0000

    Automatically backed up by Learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,12 +1,19 @@
 # Build your say_hello method here
 
 def say_hello(name = "Ruby Programmer")
+
   puts ("Hello #{name}")
 
 end
 
 def say_hello(name1)
   puts ("Hello Kent Beck")
+
+
+  puts "Hello #{name}"
+  
+  return #{name}
+  
 
 end
 
